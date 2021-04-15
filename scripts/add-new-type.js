fc458e3845eab2d5dff9ca935c004dd0ff59983f
刘文壮 <liuwenzhuang@corp.netease.com>
2021-04-15 20:02:32 +0800

    chore: addtype script support path as parameter

--- scripts/add-new-type.js
+++ scripts/add-new-type.js
@@ -1,20 +1,34 @@
 const process = require('process')
 const fs = require('fs')
 const path = require('path')
-const { kebabCase, camelCase, capitalize } = require('lodash')
+const { kebabCase, camelCase, capitalize, last, take } = require('lodash')
 
 const argv = process.argv
 
 if (argv.length < 3) {
-  console.error('usage: yarn addtype [type]')
+  console.error('usage: yarn addtype [type | path]')
   process.exit(1)
 }
 
-const initialType = argv[2]
+let initialType = argv[2].trim()
+let isPath = false
+
+const paths = initialType.split('/').filter((item) => !!item)
+const len = paths.length
+if (len > 1) {
+  // 路径形式的
+  initialType = last(paths)
+  isPath = true
+}
+
 const type = kebabCase(initialType) // 连字符的形式作为文件(夹)名
 const camelCaseType = capitalize(camelCase(initialType)) // 驼峰形式作为类名
 
-const newPath = path.join(__dirname, '../src', type)
+const newPath = path.join(
+  __dirname,
+  '../src',
+  ...(isPath ? take(paths, len - 1) : [type])
+)
 const newClsPath = path.join(newPath, `${type}.ts`)
 const newTestPath = path.join(newPath, '__tests__')
 const newTestFilePath = path.join(newTestPath, `${type}.test.ts`)
