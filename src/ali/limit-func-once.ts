3b843a8fc15f57a024b98da122372139e157b73f
刘文壮 <liuwenzhuang@corp.netease.com>
2021-05-08 18:21:47 +0800

    chore: add descriptions

--- src/ali/limit-func-once.ts
+++ src/ali/limit-func-once.ts
@@ -1,3 +1,9 @@
+/**
+ * @fileoverview
+ * 实现一个`Foo`方法，接受函数`func`和时间`wait`
+ * 返回一个新函数，新函数即时连续多次执行，但也只限制在`wait`的时间执行一次
+ */
+
 export function limitFuncOnce(func: (...args: any[]) => any, wait: number) {
   let timeId: number
 
