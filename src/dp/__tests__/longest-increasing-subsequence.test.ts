8d1f8319cfbd3e988d52d9e98d4b61b8468e2f52
刘文壮 <liuwenzhuang@corp.netease.com>
2021-08-10 20:45:14 +0800

    feat: 穷举法和动态规划获取 LIS 的序列

--- src/dp/__tests__/longest-increasing-subsequence.test.ts
+++ src/dp/__tests__/longest-increasing-subsequence.test.ts
@@ -1,6 +1,8 @@
 import {
   recursiveFindLISLength,
   dpFindLISLength,
+  bruteForceFindLIS,
+  dpFindLIS,
 } from '../longest-increasing-subsequence'
 
 describe('LIS', () => {
@@ -10,6 +12,9 @@ describe('LIS', () => {
 
     result = recursiveFindLISLength([2])
     expect(result).toBe(1)
+
+    result = recursiveFindLISLength([5, 2, 3, 10, 8, 39, 4])
+    expect(result).toBe(4)
 
     result = recursiveFindLISLength([5, 2, 3, 10, 8, 39, 28, 41])
     expect(result).toBe(5)
@@ -22,7 +27,38 @@ describe('LIS', () => {
     result = dpFindLISLength([2])
     expect(result).toBe(1)
 
+    result = dpFindLISLength([5, 2, 3, 10, 8, 39, 4])
+    expect(result).toBe(4)
+
     result = dpFindLISLength([5, 2, 3, 10, 8, 39, 28, 41])
     expect(result).toBe(5)
+  })
+
+  it('bruteForceFindLIS', () => {
+    let result = bruteForceFindLIS([])
+    expect(result).toEqual([])
+
+    result = bruteForceFindLIS([2])
+    expect(result).toEqual([2])
+
+    result = bruteForceFindLIS([5, 2, 3, 10, 8, 39, 4])
+    expect(result).toEqual([2, 3, 10, 39])
+
+    result = bruteForceFindLIS([5, 2, 3, 10, 8, 39, 28, 41])
+    expect(result).toEqual([2, 3, 10, 39, 41])
+  })
+
+  it('dpFindLIS', () => {
+    let result = dpFindLIS([])
+    expect(result).toEqual([])
+
+    result = dpFindLIS([2])
+    expect(result).toEqual([2])
+
+    result = dpFindLIS([5, 2, 3, 10, 8, 39, 4])
+    expect(result).toEqual([2, 3, 10, 39])
+
+    result = dpFindLIS([5, 2, 3, 10, 8, 39, 28, 41])
+    expect(result).toEqual([2, 3, 10, 39, 41])
   })
 })
--- src/dp/longest-increasing-subsequence.ts
+++ src/dp/longest-increasing-subsequence.ts
@@ -53,14 +53,92 @@ export function dpFindLISLength(arr: number[]) {
   }
   // 对应下标的 LIS 值
   const lisArr: number[] = Array(len).fill(1)
+  let maxLIS = 1
+  let index = 1
   for (let i = 1; i < len; i++) {
     for (let j = 0; j < i; j++) {
       // lis[i] = 0 < j < i ? 1 + max(lis[j]) : 1
       if (arr[j] < arr[i] && lisArr[i] < lisArr[j] + 1) {
         lisArr[i] = lisArr[j] + 1
+        if (lisArr[i] > maxLIS) {
+          maxLIS = lisArr[i]
+          index = i
+        }
       }
     }
   }
 
-  return lisArr[len - 1]
+  return lisArr[index]
+}
+
+/**
+ * 穷举法获取 LIS 序列
+ * 时间复杂度：O(n^2)
+ * @param arr
+ * @returns
+ */
+export function bruteForceFindLIS(arr: number[]) {
+  const len = arr.length
+  if (len < 2) {
+    return arr
+  }
+
+  let currentLISArr: number[] = []
+  let longestLISArr: number[] = []
+
+  for (let i = 0; i < len; i++) {
+    currentLISArr = []
+    let maxValue = Number.MIN_SAFE_INTEGER
+    for (let j = i; j < len; j++) {
+      if (arr[j] > maxValue) {
+        maxValue = arr[j]
+        currentLISArr.push(arr[j])
+      }
+    }
+    if (currentLISArr.length > longestLISArr.length) {
+      longestLISArr = currentLISArr
+    }
+  }
+
+  return longestLISArr
+}
+
+/**
+ * 动态规划获取 LIS 序列
+ * 时间复杂度：O(n^2)
+ * @param arr
+ */
+export function dpFindLIS(arr: number[]) {
+  const len = arr.length
+  if (len < 2) {
+    return arr
+  }
+  // 1, 3, 2, 8
+  // L(0) = [1]
+  // L(1) = [1, 3]
+  // L(2) = [1, 2]
+  // L(3) = [...maxLength(L(0), L(1), L(2)), arr[3])
+  // arr[0...i-1]都不比 arr[i] 小，此时下标 i 对应的 LIS 序列为其本身
+
+  const indexLISTuple: number[][] = [[arr[0]]]
+  // 最长的 LIS
+  let longestLIS = [arr[0]]
+  for (let i = 1; i < len; i++) {
+    // 当前循环下最长的 LIS
+    let currentLIS: number[] = []
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[i]) {
+        if (indexLISTuple[j].length > currentLIS.length) {
+          currentLIS = indexLISTuple[j]
+        }
+      }
+    }
+    indexLISTuple[i] = [...currentLIS, arr[i]]
+
+    if (indexLISTuple[i].length > longestLIS.length) {
+      longestLIS = indexLISTuple[i]
+    }
+  }
+
+  return longestLIS
 }
