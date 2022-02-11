22e464d7295a7dbec6d3eae071ac0aab26f995ff
刘文壮 <liuwenzhuang@corp.netease.com>
2022-02-11 10:05:01 +0800

    chore(linked list): optimize merge two sorted list algorithm

--- src/leetcode/linked-list/merge-two-sorted-linked-list.ts
+++ src/leetcode/linked-list/merge-two-sorted-linked-list.ts
@@ -6,47 +6,30 @@
 import { LinkedNode, logLinkedList } from '../../linked-list/linked-list'
 
 export class MergeTwoSortedLinkedList {
-  // TODO: optimize this solution
   solution(head1: LinkedNode, head2: LinkedNode) {
     let cursor1 = head1
     let cursor2 = head2
-    let newHead: LinkedNode = null
-    let newCursor: LinkedNode = null
+    let newHead: LinkedNode = new LinkedNode(0) // fake head
+    let newCursor: LinkedNode = newHead
 
-    while (cursor1 || cursor2) {
-      const value1 = cursor1?.value ?? Number.MAX_SAFE_INTEGER
-      const value2 = cursor2?.value ?? Number.MAX_SAFE_INTEGER
+    while (cursor1 && cursor2) {
+      const value1 = cursor1.value
+      const value2 = cursor2.value
 
       if (value1 < value2) {
-        if (!newHead) {
-          newCursor = newHead = new LinkedNode(value1)
-        } else {
-          newCursor = newCursor.append(value1)
-        }
+        newCursor.next = cursor1
         cursor1 = cursor1.next
-      }
-
-      if (value1 > value2) {
-        if (!newHead) {
-          newCursor = newHead = new LinkedNode(value2)
-        } else {
-          newCursor = newCursor.append(value2)
-        }
+      } else {
+        newCursor.next = cursor2
         cursor2 = cursor2.next
       }
-
-      if (value1 === value2) {
-        if (!newHead) {
-          newCursor = newHead = new LinkedNode(value1)
-        } else {
-          newCursor = newCursor.append(value1)
-        }
-        newCursor = newCursor.append(value2)
-        cursor1 = cursor1.next
-        cursor2 = cursor2.next
-      }
+      newCursor = newCursor.next
     }
 
-    return logLinkedList(newHead)
+    // linkedlist1 或 linkedlist2 中未添加完的
+    newCursor.next = cursor1 || cursor2
+
+    // 因为 newHead 是新增的节点，需要去除
+    return logLinkedList(newHead.next)
   }
 }
