3d42744e0364c38cf740a44c3183af2d89ed43d5
刘文壮 <liuwenzhuang@corp.netease.com>
2022-02-14 14:21:08 +0800

    feat(linked list): merge n sorted linked list by divide and conquer

--- src/leetcode/linked-list/__tests__/merge-n-sorted-linked-list.test.ts
+++ src/leetcode/linked-list/__tests__/merge-n-sorted-linked-list.test.ts
@@ -14,4 +14,15 @@ describe('MergeNSortedLinkedList', () => {
       ])
     ).toEqual([1, 1, 2, 3, 4, 4, 5, 6])
   })
+
+  it('MergeNSortedLinkedList.solution should work properly', () => {
+    expect(mnsll.solution([])).toEqual([])
+    expect(
+      mnsll.solution([
+        new LinkedList([1, 4, 5]).head,
+        new LinkedList([1, 3, 4]).head,
+        new LinkedList([2, 6]).head,
+      ])
+    ).toEqual([1, 1, 2, 3, 4, 4, 5, 6])
+  })
 })
--- src/leetcode/linked-list/merge-n-sorted-linked-list.ts
+++ src/leetcode/linked-list/merge-n-sorted-linked-list.ts
@@ -7,6 +7,29 @@ import { LinkedNode, logLinkedList } from '../../linked-list/linked-list'
 import { MergeTwoSortedLinkedList } from './merge-two-sorted-linked-list'
 
 export class MergeNSortedLinkedList {
+  mtsl = new MergeTwoSortedLinkedList()
+
+  /**
+   * 使用分治算法，减少 {@link MergeNSortedLinkedList.firstSolution} 中合并的次数
+   * O(log2k * N)
+   * @param lists
+   */
+  solution(lists: LinkedNode<number>[]) {
+    const loop = (arr: LinkedNode<number>[]) => {
+      const len = arr.length
+      if (len < 2) {
+        return arr[0] ?? null
+      }
+      const mid = Math.floor(len / 2)
+      return this.mtsl.solution(
+        loop(arr.slice(0, mid)),
+        loop(arr.slice(mid)),
+        false
+      )
+    }
+    return logLinkedList(loop(lists))
+  }
+
   /**
    * 两两合并，借助 {@link MergeTwoSortedLinkedList.solution } 中的合并算法
    * O(N * (k - 1)) = O(kN)
@@ -15,9 +38,8 @@ export class MergeNSortedLinkedList {
    */
   firstSolution(lists: LinkedNode<number>[]) {
     let result = lists[0] ?? null
-    const mtsl = new MergeTwoSortedLinkedList()
     for (let i = 1, len = lists.length; i < len; i++) {
-      result = mtsl.solution(result, lists[i], false) as LinkedNode<number>
+      result = this.mtsl.solution(result, lists[i], false) as LinkedNode<number>
     }
     return logLinkedList(result)
   }
